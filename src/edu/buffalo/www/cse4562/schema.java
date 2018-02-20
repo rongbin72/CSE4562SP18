994d8d87610ecff7f4031ff9e5a667757da8dbb8
Robin <bin.googol@gmail.com>
2018-02-20 16:32:56 -0500

    schema.java

---
+++ src/edu/buffalo/www/cse4562/schema.java
@@ -0,0 +1,26 @@
+package edu.buffalo.www.cse4562;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class schema {
+    private String tableName;
+    private String tablePath;
+    private Map<String, Integer> colNumber;
+    private Map<String, String> colType;
+
+    public schema() {
+
+    }
+    public String getPath(String colName) {
+
+    }
+
+    public String getColType(String colName) {
+
+    }
+
+    public int getColNumber(String colName) {
+
+    }
+}
