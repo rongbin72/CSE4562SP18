5ab68c9b38b9f4563326b26efdfc1d4fe5f089a2
martinchia <martinchia93@outlook.com>
2018-02-21 04:16:32 -0500

    003

--- src/edu/buffalo/www/cse4562/SelectObject.java
+++ src/edu/buffalo/www/cse4562/SelectObject.java
@@ -1,5 +1,6 @@
 package edu.buffalo.www.cse4562;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import net.sf.jsqlparser.expression.*;
@@ -9,9 +10,12 @@ public class SelectObject implements SelectItemVisitor{
 	private List<SelectItem> items;
 	private List<String> tuple;
 	private List<String> tempResult = null;
+	private Schema schema;
+	private PrimitiveValue evalResult;
 	
 	public SelectObject(List<SelectItem> list,Schema schema) {
 		this.items = list;
+		this.schema = schema;
 	}
 	
 	public List<String> Result(List<String> tuple) {
@@ -37,7 +41,11 @@ public class SelectObject implements SelectItemVisitor{
 	@Override
 	public void visit(SelectExpressionItem exp) {
 		Expression e = exp.getExpression();
-		//need to get the expression value
-		
+		Evaluation eval = new Evaluation(this.schema,this.tuple);
+		try {
+			this.evalResult = eval.eval(e);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
 	}
 }
