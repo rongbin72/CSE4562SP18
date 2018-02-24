f2aeb2d61fb54696aa7d04da8d5676c2a5e147db
Robin <bin.googol@gmail.com>
2018-02-24 13:13:40 -0500

    Remove redundant variable

--- src/edu/buffalo/www/cse4562/Read.java
+++ src/edu/buffalo/www/cse4562/Read.java
@@ -2,11 +2,9 @@ package edu.buffalo.www.cse4562;
 import java.io.*;
 
 public class Read {
-	private File f;
 	private BufferedReader br;
 
 	public Read(File f) throws FileNotFoundException {
-		this.f = f;
 		FileReader fr = new FileReader(f);
 		this.br = new BufferedReader(fr);
 	}
