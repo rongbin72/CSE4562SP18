0d8db9e77963669f42c5bdaa636f1bc640c0cf8a
Robin <bin.googol@gmail.com>
2018-03-07 21:03:57 -0500

    remove unnecessary string comparsion

--- src/edu/buffalo/www/cse4562/WhereObject.java
+++ src/edu/buffalo/www/cse4562/WhereObject.java
@@ -9,23 +9,19 @@ public class WhereObject {
 	private Expression where;
 	private Schema schema;
 	private String tablename;
-	
+
 	public WhereObject(Expression where, Schema schema) {
 		this.where = where;
 		this.schema = schema;
 	}
-	
+
 	public void setTable(String newTable) {
 		this.tablename = newTable;
 	}
 
-	public boolean Result(List<String> tuple) throws SQLException{
-	    Evaluation eval = new Evaluation(this.schema,this.tablename, tuple);
-	    PrimitiveValue result = eval.eval(this.where);
-	    if(result.toString().equals("TRUE")) {
-	        return true;
-        } else {
-	        return false;
-        }
+	public boolean Result(List<String> tuple) throws SQLException {
+		Evaluation eval = new Evaluation(this.schema, this.tablename, tuple);
+		PrimitiveValue result = eval.eval(this.where);
+		return result.toBool();
 	}
 }
