851737273995dd47f031067844f9b0ff39632937
Robin <bin.googol@gmail.com>
2018-03-08 19:36:37 -0500

    static schema

--- src/edu/buffalo/www/cse4562/Schema.java
+++ src/edu/buffalo/www/cse4562/Schema.java
@@ -1,10 +1,12 @@
 package edu.buffalo.www.cse4562;
 
+import net.sf.jsqlparser.expression.PrimitiveValue;
 import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 import net.sf.jsqlparser.statement.create.table.CreateTable;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.PrimitiveIterator;
 
 public class Schema {
     private String tableName;
@@ -19,10 +21,40 @@ public class Schema {
 //     * Add a table definition to schema
 //     * @param table
 //     */
-//    public static void addTable(CreateTable table) {
-//        TableDef t = new TableDef(table);
-//        schema.put(t.getTableName(), t);
-//    }
+
+    public static void addTable(CreateTable table) {
+        TableDef t = new TableDef(table);
+        schema.put(t.getTableName(), t);
+    }
+
+    public static void addTable(String tableName, HashMap<String, Integer> colIndex, List<List<PrimitiveValue>> table) {
+
+    }
+
+    public static void addColumn(String tableName, String colName) {
+
+    }
+
+    public static HashMap<String, Integer> getIndxHash(String tableName) {
+
+    }
+
+    public static List<List<PrimitiveValue>> getTableContent(String tableName) {
+
+    }
+
+    /**
+     * Get relative path of the table
+     *
+     * @param tableName name of table
+     * @return table path
+     */
+    public String getPath(String tableName) {
+        return this.tablePath.get(tableName);
+    }
+
+
+
 //
 //    /**
 //     * Get table path by table name
@@ -59,112 +91,104 @@ public class Schema {
      *
      * @param schema
      */
-    public void init(CreateTable schema) throws NullPointerException {
-        this.tableName = schema.getTable().getName();
-        this.tablePath.put(this.tableName, "data/" + this.tableName + ".dat");
-
-        int columnIndex = 0;  // column index, starting from 0
-        HashMap<String, Integer> tempColIndex = new HashMap<>();
-        HashMap<Integer, String> tempTypeByIndex = new HashMap<>();
-        HashMap<String, String> tempTypeByName = new HashMap<>();
-        for (ColumnDefinition col : schema.getColumnDefinitions()) {
-            // temp variables to be put
-            String colName = col.getColumnName();
-            String colType = col.getColDataType().getDataType();
-            tempColIndex.put(colName, columnIndex);
-            tempTypeByIndex.put(columnIndex, col.getColDataType().getDataType());
-            tempTypeByName.put(colName, colType);
-            columnIndex++;
-        }
-        this.colIndex.put(tableName, tempColIndex);
-        this.typeByIndex.put(tableName, tempTypeByIndex);
-        this.typeByName.put(tableName, tempTypeByName);
-    }
-
-    /**
-     * Construction overload
-     *
-     * @param colIndex  map of <code>{column_name: column_index}</code>
-     * @param colType   map of <code>{column_index: column_type}</code>
-     * @param tableName table name
-     */
-    public void init(HashMap<String, Integer> colIndex, HashMap<String, String> colType, String tableName) {
-        this.colIndex.put(tableName, colIndex);
-        this.typeByName.put(tableName, colType);
-    }
-
-    /**
-     * @return table name
-     */
-    public String getTableName() {
-        return this.tableName;
-    }
-
-    /**
-     * Get relative path of the table
-     *
-     * @param tableName name of table
-     * @return table path
-     */
-    public String getPath(String tableName) {
-        return this.tablePath.get(tableName);
-    }
-
-    /**
-     * Get column type by table name and column index
-     *
-     * @param tableName name of table
-     * @param colIndex  index of the column
-     * @return type of the column
-     */
-    public String getColType(String tableName, int colIndex) {
-
-        return this.typeByIndex.get(tableName).get(colIndex);
-    }
-
-    /**
-     * Overload
-     * Get column type by table name and column name
-     *
-     * @param tableName table name
-     * @param colName column name
-     * @return type of the column
-     */
-    public String getColType(String tableName, String colName) {
-        return this.typeByName.get(tableName).get(colName);
-    }
-
-    /**
-     * Get column index by table name and column name
-     *
-     * @param tableName name of table
-     * @param colName   name of the column
-     * @return index of the column
-     */
-    public int getColIndex(String tableName, String colName) {
-        int i = this.colIndex.get(tableName).get(colName);
-        return i;
-
-    }
-
-    /**
-     * Return the map of <code>{column_name: column_type}</code> by table name
-     *
-     * @param tableName table name
-     * @return map of <code>{column_name: column_index}</code>
-     */
-    public HashMap<String, Integer> getIndex(String tableName) {
-        return this.colIndex.get(tableName);
-    }
-
-    /**
-     * Return the map of <code>{column_index: column_type}</code> by table name
-     *
-     * @param tableName table name
-     * @return map of <code>{column_index: column_type}</code>
-     */
-    public HashMap<String, String> getType(String tableName) {
-        return this.typeByName.get(tableName);
-    }
+//    public void init(CreateTable schema) throws NullPointerException {
+//        this.tableName = schema.getTable().getName();
+//        this.tablePath.put(this.tableName, "data/" + this.tableName + ".dat");
+//
+//        int columnIndex = 0;  // column index, starting from 0
+//        HashMap<String, Integer> tempColIndex = new HashMap<>();
+//        HashMap<Integer, String> tempTypeByIndex = new HashMap<>();
+//        HashMap<String, String> tempTypeByName = new HashMap<>();
+//        for (ColumnDefinition col : schema.getColumnDefinitions()) {
+//            // temp variables to be put
+//            String colName = col.getColumnName();
+//            String colType = col.getColDataType().getDataType();
+//            tempColIndex.put(colName, columnIndex);
+//            tempTypeByIndex.put(columnIndex, col.getColDataType().getDataType());
+//            tempTypeByName.put(colName, colType);
+//            columnIndex++;
+//        }
+//        this.colIndex.put(tableName, tempColIndex);
+//        this.typeByIndex.put(tableName, tempTypeByIndex);
+//        this.typeByName.put(tableName, tempTypeByName);
+//    }
+//
+//    /**
+//     * Construction overload
+//     *
+//     * @param colIndex  map of <code>{column_name: column_index}</code>
+//     * @param colType   map of <code>{column_index: column_type}</code>
+//     * @param tableName table name
+//     */
+//    public void init(HashMap<String, Integer> colIndex, HashMap<String, String> colType, String tableName) {
+//        this.colIndex.put(tableName, colIndex);
+//        this.typeByName.put(tableName, colType);
+//    }
+//
+//    /**
+//     * @return table name
+//     */
+//    public String getTableName() {
+//        return this.tableName;
+//    }
+//
+//
+//
+//    /**
+//     * Get column type by table name and column index
+//     *
+//     * @param tableName name of table
+//     * @param colIndex  index of the column
+//     * @return type of the column
+//     */
+//    public String getColType(String tableName, int colIndex) {
+//
+//        return this.typeByIndex.get(tableName).get(colIndex);
+//    }
+//
+//    /**
+//     * Overload
+//     * Get column type by table name and column name
+//     *
+//     * @param tableName table name
+//     * @param colName column name
+//     * @return type of the column
+//     */
+//    public String getColType(String tableName, String colName) {
+//        return this.typeByName.get(tableName).get(colName);
+//    }
+//
+//    /**
+//     * Get column index by table name and column name
+//     *
+//     * @param tableName name of table
+//     * @param colName   name of the column
+//     * @return index of the column
+//     */
+//    public int getColIndex(String tableName, String colName) {
+//        int i = this.colIndex.get(tableName).get(colName);
+//        return i;
+//
+//    }
+//
+//    /**
+//     * Return the map of <code>{column_name: column_type}</code> by table name
+//     *
+//     * @param tableName table name
+//     * @return map of <code>{column_name: column_index}</code>
+//     */
+//    public HashMap<String, Integer> getIndex(String tableName) {
+//        return this.colIndex.get(tableName);
+//    }
+//
+//    /**
+//     * Return the map of <code>{column_index: column_type}</code> by table name
+//     *
+//     * @param tableName table name
+//     * @return map of <code>{column_index: column_type}</code>
+//     */
+//    public HashMap<String, String> getType(String tableName) {
+//        return this.typeByName.get(tableName);
+//    }
 
 }
