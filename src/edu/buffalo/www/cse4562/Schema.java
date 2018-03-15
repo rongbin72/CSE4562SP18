3a2d009db2db992dcae12fec3d3265cdf0e578af
Robin <bin.googol@gmail.com>
2018-03-15 16:26:03 -0400

    Fix reset()

--- src/edu/buffalo/www/cse4562/Schema.java
+++ src/edu/buffalo/www/cse4562/Schema.java
@@ -6,37 +6,66 @@ import net.sf.jsqlparser.statement.create.table.CreateTable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 public class Schema {
     private static HashMap<String, TableDef> schema = new HashMap<>();
     private static List<String> extraTable = new ArrayList<>();
-    private static HashMap<String, String> extraCol = new HashMap<>();
+    private static List<String> extraCol = new ArrayList<>(); // store names of table which has extra column
 
     /**
-     * Add a table definition to schema
+     * Add a table definition to schema when create table
      * @param table
      */
     public static void addTable(CreateTable table) {
         schema.put(table.getTable().getName().toUpperCase(), new TableDef(table));
     }
 
+    /**
+     * Call this method when subselect, will be deleted by reset
+     * @param tableName subselect table name
+     * @param colIndex column index
+     * @param table subselect table content
+     */
     public static void addTable(String tableName, HashMap<String, Integer> colIndex, List<List<PrimitiveValue>> table) {
         schema.put(tableName.toUpperCase(), new TableDef(tableName, colIndex, table));
         extraTable.add(tableName.toUpperCase());
     }
 
+    /**
+     * When table alias, will be deleted by reset.
+     * Will copy every attributes except for table name and table content
+     * @param oldName the name of table to be copy
+     * @param newName new table name -> alias
+     */
     public static void addTable(String oldName, String newName) {
-        schema.put(newName.toUpperCase(), schema.get(oldName));
+        // copy old tableDef except table
+        String path = schema.get(oldName).getTablePath();
+        HashMap<String, Integer> colIndex = schema.get(oldName).getIndexHash();
+        HashMap<Integer, String> colType = schema.get(oldName).getTypeHash();
+        TableDef t = new TableDef(newName, path, colIndex, colType);
+        schema.put(newName.toUpperCase(), t);
         extraTable.add(newName.toUpperCase());
     }
 
+    /**
+     * Schema of final output table, will be deleted by reset
+     * @param tableName *
+     * @param colIndex column index of final output table
+     */
     public static void addTable(String tableName, HashMap<String, Integer> colIndex) {
         schema.put(tableName.toUpperCase(), new TableDef(tableName, colIndex));
+        extraTable.add(tableName);
     }
 
+    /**
+     * Add new column when alias, will be deleted by reset
+     * @param tableName table name
+     * @param colName alias
+     */
     public static void addColumn(String tableName, String colName) {
         schema.get(tableName.toUpperCase()).addColumn(colName.toUpperCase());
-        extraCol.put(tableName.toUpperCase(), colName.toUpperCase());
+        extraCol.add(tableName.toUpperCase());
     }
 
     public static void setTableContent(String tableName, List<List<PrimitiveValue>> tableContent) {
@@ -84,8 +113,8 @@ public class Schema {
     		extraTable.clear();
     	}        
     	else {
-            for (String c : extraCol.keySet()) {
-                schema.get(c.toUpperCase()).removeCol(extraCol.get(c.toUpperCase()));
+            for (String c : extraCol) {
+                schema.get(c.toUpperCase()).removeCol();
             } 
             extraCol.clear();
     	}
