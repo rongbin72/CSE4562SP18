6409f83425213bef17e7be35bb8709d9dec070e2
martin chia <martinchia93@outlook.com>
2018-04-06 15:17:09 -0400

    000

--- src/edu/buffalo/www/cse4562/Operator.java
+++ src/edu/buffalo/www/cse4562/Operator.java
@@ -3,7 +3,7 @@ package edu.buffalo.www.cse4562;
 import java.io.IOException;
 
 public abstract class Operator {
-	public abstract Tuple result() throws IOException;
+	public abstract Tuple result();
 	public Operator() {
 		
 	}
