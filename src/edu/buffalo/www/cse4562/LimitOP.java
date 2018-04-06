c9fda882523ea4dd26fff4e3d6dc9de2a8e068df
Robin <bin.googol@gmail.com>
2018-04-05 21:29:40 -0400

    RAtree

--- src/edu/buffalo/www/cse4562/LimitOP.java
+++ src/edu/buffalo/www/cse4562/LimitOP.java
@@ -3,8 +3,12 @@ package edu.buffalo.www.cse4562;
 import net.sf.jsqlparser.statement.select.Limit;
 
 public class LimitOP extends Operator{
-    public LimitOP(Operator op, Limit limit) {
+    private Operator son;
+    private Limit limit;
 
+    public LimitOP(Operator op, Limit limit) {
+        this.son = op;
+        this.limit = limit;
     }
 
     @Override
--- src/edu/buffalo/www/cse4562/OrderbyOP.java
+++ src/edu/buffalo/www/cse4562/OrderbyOP.java
@@ -1,12 +1,16 @@
 package edu.buffalo.www.cse4562;
 
+import com.sun.jmx.remote.util.OrderClassLoaders;
 import net.sf.jsqlparser.statement.select.OrderByElement;
 
 import java.util.List;
 
 public class OrderbyOP extends Operator {
+    private Operator son;
+    private List<OrderByElement> orderBy;
     public OrderbyOP(Operator op, List<OrderByElement> orderBy) {
-
+        this.son = op;
+        this.orderBy = orderBy;
     }
     @Override
 
