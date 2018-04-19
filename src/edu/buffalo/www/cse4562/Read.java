0aecd20fabc2ddad8449c595849ed9a036cbfdb4
Robin <bin.googol@gmail.com>
2018-04-18 23:23:28 -0400

    Read 100 line at a time

--- src/edu/buffalo/www/cse4562/Read.java
+++ src/edu/buffalo/www/cse4562/Read.java
@@ -4,59 +4,76 @@ import net.sf.jsqlparser.expression.PrimitiveValue;
 import net.sf.jsqlparser.schema.Table;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 
-public class Read extends Operator{
-	private String tableName;
-	private String path;
-	private boolean eof = false;
-	private boolean isFirstLine = true;
-	//	private Integer bufferSize = 10000000;
-//	private double factor = 0.5;
-//	private Queue<List<PrimitiveValue>> buffer = new LinkedList<>();
-	private List<List<PrimitiveValue>> table = new ArrayList<>();
-	private Iterator<List<PrimitiveValue>> tableIterator;
+public class Read extends Operator {
+    private String tableName;
+    private String path;
+    private boolean eof = false;
+    private boolean isFirstLine = true;
+    private Integer bufferSize = 100;
+    private BufferedReader br;
+    private double factor = 0.5;
+    private Queue<List<PrimitiveValue>> buffer = new LinkedList<>();
+    private List<List<PrimitiveValue>> table = new ArrayList<>();
+    private Iterator<List<PrimitiveValue>> tableIterator;
 
-//	private void fillBuffer() throws IOException {
-//		String line;
-//		while ((line = br.readLine()) != null) {
-//			this.buffer.add(Helper.toPrimitive(this.tableName, line));
-//			if (this.buffer.size() == this.bufferSize) {
-//				break;
-//			}
-//		}
-//		if (this.buffer.size() < bufferSize) {
-//			this.eof = true;
-//		}
-//	}
-	
-	public String getTableName() {
-		return this.tableName;
-	}
+    public Read(Table table) {
+        this.tableName = table.getName();
+        this.path = Schema.getPath(tableName);
+        FileInputStream fs = null;
+        try {
+            fs = new FileInputStream(new File(this.path));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        this.br = new BufferedReader(new InputStreamReader(fs));
+        try {
+            fillBuffer();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public Read(Table table) {
-		this.tableName = table.getName();
-		this.path = Schema.getPath(tableName);
-	}
+    private void fillBuffer() throws IOException {
+        String line;
+        while ((line = this.br.readLine()) != null) {
+            this.buffer.add(Helper.toPrimitive(this.tableName, line));
+            if (this.buffer.size() == this.bufferSize) {
+                break;
+            }
+        }
+        if (this.buffer.size() < bufferSize) {
+            this.eof = true;
+        }
+    }
 
-	private void init() throws IOException {
-		FileInputStream fs = new FileInputStream(new File(this.path));
-		BufferedReader br = new BufferedReader(new InputStreamReader(fs));
-		String line;
-		while ((line = br.readLine()) != null) {
-			this.table.add(Helper.toPrimitive(this.tableName, line));
-		}
-		fs.close();
-		br.close();
-		this.tableIterator = this.table.iterator();
-	}
+    public String getTableName() {
+        return this.tableName;
+    }
 
-	@Override
-	public Tuple result() {
-		// fill buffer
+    private void init() throws IOException {
+        if (!this.tableName.equals("PLAYERS")) {
+            FileInputStream fs = new FileInputStream(new File(this.path));
+            BufferedReader br = new BufferedReader(new InputStreamReader(fs));
+            String line;
+            while ((line = br.readLine()) != null) {
+                this.table.add(Helper.toPrimitive(this.tableName, line));
+            }
+            fs.close();
+            br.close();
+            this.tableIterator = this.table.iterator();
+        } else {
+            FileInputStream fs = new FileInputStream(new File(this.path));
+            this.br = new BufferedReader(new InputStreamReader(fs));
+            fillBuffer();
+        }
+    }
+
+    @Override
+    public Tuple result() {
+        // fill buffer
 //		if (!eof && this.buffer.size() <= this.bufferSize * this.factor) {
 //			try {
 //				fillBuffer();
@@ -73,34 +90,56 @@ public class Read extends Operator{
 //				e.printStackTrace();
 //			}
 //			return null;
-		if (isFirstLine) {
-			try {
-				init();
-				isFirstLine = false;
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			if (this.tableIterator.hasNext()) {
-				return new Tuple(tableName, new ArrayList<>(tableIterator.next()));
-			}
-		}
+        if (!this.tableName.equals("PLAYERS")) {
+            if (isFirstLine) {
+                try {
+                    init();
+                    isFirstLine = false;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (this.tableIterator.hasNext()) {
+                    return new Tuple(tableName, new ArrayList<>(tableIterator.next()));
+                }
+            }
 
-		if (this.tableIterator.hasNext()) {
-			return new Tuple(tableName, new ArrayList<>(tableIterator.next())) ;
-		} else {
-			this.tableIterator = this.table.iterator();
-			return null;
-		}
-	}
+            if (this.tableIterator.hasNext()) {
+                return new Tuple(tableName, new ArrayList<>(tableIterator.next()));
+            } else {
+                this.tableIterator = this.table.iterator();
+                return null;
+            }
+        } else {
+//			 fill buffer
+            if (!eof && this.buffer.size() <= this.bufferSize * this.factor) {
+                try {
+                    fillBuffer();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
-	@Override
-	public Operator getSon() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+            if (eof && buffer.size() == 0) {
+                try {
+                    this.br.close();
+                    init();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+            return new Tuple(tableName, this.buffer.poll());
+        }
+    }
 
-	@Override
-	public void setSon(Operator son) {
-		
-	}
+    @Override
+    public Operator getSon() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setSon(Operator son) {
+
+    }
 }
